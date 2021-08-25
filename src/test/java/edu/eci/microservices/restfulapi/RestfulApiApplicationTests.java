d62c31268ac43b07f4caa47f5bb87c22cbe73e2b
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-08-25 10:34:22 -0500

    v.1 con punto 1 desarrollado.

---
+++ src/test/java/edu/eci/microservices/restfulapi/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.microservices.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
